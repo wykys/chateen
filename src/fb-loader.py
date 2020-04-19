10ce0e1e81271807ace80179e2dc5f777046f661
Jan Vykydal <john.vykydal@gmail.com>
2020-04-19 02:27:50 +0200

    Created fb loader

---
+++ src/fb-loader.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# wykys 2020
+# program pro vytváření korpusů z Facebook JSON
+
+import re
+import json
+
+
+class Corpus(object):
+    def __init__(self, author: str):
+        self.author = author
+        self.messages = []
+
+    def add(self, item: str):
+        self.messages.append(f'<s>{item}</s>\n')
+
+    def save(self):
+        with open(f'../out/{self.author}.txt', 'w') as fw:
+            fw.writelines(self.messages)
+
+
+class FbLoader(object):
+    def __init__(self, path='../data/message_1.json'):
+        self.path = path
+        self.load()
+        self.decode()
+
+    def load(self):
+        def fix_fb_code(obj):
+            def fix(s: str) -> str:
+                return s.encode('latin_1').decode('utf-8')
+
+            for key in obj:
+                if isinstance(obj[key], str):
+                    obj[key] = fix(obj[key])
+                elif isinstance(obj[key], list):
+                    obj[key] = list(
+                        map(
+                            lambda x:
+                                fix(x) if isinstance(x, str) else x,
+                                obj[key]
+                        )
+                    )
+            return obj
+
+        with open(self.path, 'r') as fr:
+            data = json.load(fr, object_hook=fix_fb_code)
+
+        self.chat = data['messages']
+        self.authors = tuple(map(lambda x: x['name'], data['participants']))
+        self.corpus = dict()
+        for author in self.authors:
+            self.corpus[author] = Corpus(author)
+
+    def decode(self):
+        for message in self.chat:
+            if 'content' in message and 'sender_name' in message:
+                self.corpus[message['sender_name']].add(message['content'])
+
+    def save(self):
+        for name in self.corpus:
+            self.corpus[name].save()
+
+
+if __name__ == '__main__':
+    loader = FbLoader()
+    loader.save()
+
