2022e0a7e334cad7de76fe0fef0cf4aaed243c8d
Jan Vykydal <john.vykydal@gmail.com>
2020-04-20 23:10:38 +0200

    Added script for creating virtual enviroment

---
+++ src/venv.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# create and install python enviroment
+# wykys 2018
+
+SCRIPTS_DIR=.scripts
+
+if [ -d ".venv" ]; then
+    echo "remove old venv"
+    rm .venv -rf
+fi
+
+echo "create new venv"
+python3.8 -m venv .venv
+
+echo "activate venv"
+. .venv/bin/activate
+
+echo "upgrade pip"
+pip install --upgrade pip
+
+echo "install modules from requirements"
+pip install -r requirements.txt
+
+echo "installed modules"
+pip freeze
