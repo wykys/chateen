ebfe31b953dcab6345f8aebd1ea2584111ab4839
Jan Vykydal <john.vykydal@gmail.com>
2020-04-26 01:31:46 +0200

    Separated html to own folder

--- src/chateen.py
+++ src/chateen.py
@@ -142,7 +142,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.load_new_data()
 
     def show_html(self, path):
-        with open(path, 'r') as fr:
+        with open(f'../html/{path}', 'r') as fr:
             html = fr.read()
         self.text_more.setText('')
         self.text_more.insertHtml(html)
